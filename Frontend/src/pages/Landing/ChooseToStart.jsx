53d2d90a48697d97305917e11eb3dd44741f1d7e
Debdeep <dbworkofficial@gmail.com>
2025-12-24 19:55:00 +0530

    hello

--- Frontend/src/pages/Landing/ChooseToStart.jsx
+++ Frontend/src/pages/Landing/ChooseToStart.jsx
@@ -64,7 +64,7 @@ const ChooseToStart = () => {
         className="w-full flex items-center justify-center gap-4 sm:gap-8 md:gap-12"
         ref={scrollRef}
       >
-        <div className="scroll-box w-32 h-32 md:w-66 md:h-66 rotate-45 md:rotate-[5deg] rounded-2xl bg-gradient-to-br from-pink-400 to-fuchsia-500 shadow-lg brightness-90 flex items-center justify-center">
+        <div className="scroll-box w-32 h-32 md:w-66 md:h-66 rotate-15 md:rotate-[5deg] rounded-2xl bg-gradient-to-br from-pink-400 to-fuchsia-500 shadow-lg brightness-90 flex items-center justify-center">
           <div className="hover:scale-110 transition-all duration-200 active:scale-90 rotate-[20deg] md:rotate-[47deg] flex flex-col items-center">
             <Store className="w-18 h-18 sm:w-24 sm:h-24 text-fuchsia-950" />
             <span className="text-lg sm:text-2xl md:text-3xl font-extrabold tracking-wide text-fuchsia-950 font-[cursive]">
@@ -73,7 +73,7 @@ const ChooseToStart = () => {
           </div>
         </div>
 
-        <div className="scroll-box w-32 h-32 md:w-66 md:h-66 rotate-45 md:rotate-[5deg] rounded-2xl bg-gradient-to-br from-orange-400 to-amber-500 shadow-lg brightness-90 flex items-center justify-center">
+        <div className="scroll-box w-32 h-32 md:w-66 md:h-66 rotate-15 md:rotate-[5deg] rounded-2xl bg-gradient-to-br from-orange-400 to-amber-500 shadow-lg brightness-90 flex items-center justify-center">
           <div className="hover:scale-110 transition-all duration-200 active:scale-90 rotate-[20deg] md:rotate-[47deg] flex flex-col items-center">
             <ShoppingBag className="w-18 h-18 sm:w-24 sm:h-24 text-amber-900" />
             <span className="text-lg sm:text-2xl md:text-3xl font-extrabold tracking-wide text-amber-900 font-[cursive]">
