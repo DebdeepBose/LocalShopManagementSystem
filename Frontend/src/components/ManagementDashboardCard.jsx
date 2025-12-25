f9c6292eeddf5c51d0baf74bf324f14fd8ca1f10
Debdeep <dbworkofficial@gmail.com>
2025-12-25 16:39:07 +0530

    Size

--- Frontend/src/components/ManagementDashboardCard.jsx
+++ Frontend/src/components/ManagementDashboardCard.jsx
@@ -3,21 +3,11 @@ import React from "react";
 const ManagementDashboardCard = ({ text, icons }) => {
   return (
     <div
-      className="
-        group relative w-80 h-52 md:w-88 md:h-56 bg-violet-400 flex items-center justify-center text-[20px] sm:text-[22px] md:text-[25px] font-bold rounded-[15px] cursor-pointer
-        shadow-[4px_6px_15px_rgba(0,0,0,0.2),6px_10px_20px_rgba(0,0,0,0.15)]
-        transform hover:-translate-y-2 hover:scale-105 transition-all duration-500
-        before:content-[''] before:absolute before:top-0 before:right-0 before:w-[20%] before:h-[20%] before:bg-violet-600 before:rounded-[0_15px_0_100%] before:transition-all before:duration-500
-        after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-[20%] after:h-[20%] after:bg-violet-600 after:rounded-[0_100%_0_15px] after:transition-all after:duration-500
-        hover:before:w-full hover:before:h-full hover:before:rounded-[15px]
-        hover:after:w-full hover:after:h-full hover:after:rounded-[15px]"
-    >
-      {/* Icon */}
+      className="group active:scale-95 relative w-56 h-52 md:w-88 md:h-56 bg-violet-400 flex items-center justify-center text-[20px] sm:text-[22px] md:text-[25px] font-bold rounded-[15px] cursor-pointer shadow-[4px_6px_15px_rgba(0,0,0,0.2),6px_10px_20px_rgba(0,0,0,0.15)] transform hover:-translate-y-2 hover:scale-105 transition-all duration-500 before:content-[''] before:absolute before:top-0 before:right-0 before:w-[20%] before:h-[20%] before:bg-violet-600 before:rounded-[0_15px_0_100%] before:transition-all before:duration-500 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-[20%] after:h-[20%] after:bg-violet-600 after:rounded-[0_100%_0_15px] after:transition-all after:duration-500 hover:before:w-full hover:before:h-full hover:before:rounded-[15px] hover:after:w-full hover:after:h-full hover:after:rounded-[15px]">
       <span className="relative z-20 group-hover:opacity-0 transition-opacity duration-500 text-white">
         {icons && React.isValidElement(icons) ? icons : null}
       </span>
 
-      {/* Centered text on hover */}
       <div className="absolute inset-0 flex items-center justify-center text-white text-2xl font-bold opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-30">
         {text}
       </div>
